9a7f74990c18c0aafdb681a2fdd13f58514575e2
vdeoGit <joshi.madhura69@gmail.com>
2023-07-26 20:10:21 +0530

    Removed the account name

--- src/com/javatpoint/feereport/AccountantDao.java
+++ src/com/javatpoint/feereport/AccountantDao.java
@@ -12,7 +12,7 @@ public class AccountantDao {
 		Connection con=null;
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/feereport","joshimd","Vaibhudu@123");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/feereport","","");
 		}catch(Exception e){System.out.println(e);}
 		return con;
 	}
